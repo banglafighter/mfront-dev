d0cc688589fe0f965d8ba83d0ee8746f67daade4
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-03-31 22:07:21 +0600

    Bismillah of Project setup

---
+++ application/starter/vite.config.ts
@@ -0,0 +1,43 @@
+import {defineConfig, loadEnv} from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
+
+export default defineConfig(({mode}) => {
+    const envFolder = path.resolve(__dirname, '../env')
+    const env = loadEnv(mode, envFolder, '')
+
+    const defineEnv = Object.fromEntries(
+        Object.entries(env)
+            .filter(([key]) => /^[A-Z0-9_]+$/.test(key))
+            .map(([key, value]) => [key, JSON.stringify(value)])
+    )
+
+    return {
+        base: '/',
+        plugins: [tsconfigPaths()],
+
+        server: {
+            port: Number(env.PORT) || 1240,
+            open: true
+        },
+        resolve: {
+            preserveSymlinks: true
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    entryFileNames: 'assets/[name].[hash].js',
+                    chunkFileNames: 'assets/[name].[hash].js',
+                    assetFileNames: 'assets/[name].[hash].[ext]',
+                    manualChunks(id) {  // Need to improve it
+                        if (id.includes('node_modules')) {
+                            return 'vendor'
+                        }
+                    }
+                }
+            }
+        },
+
+        define: defineEnv
+    }
+})
