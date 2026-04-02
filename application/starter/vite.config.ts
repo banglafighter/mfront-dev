b6836a5e0ce05503a75ed2ad54bd4cf229f6ec2d
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-04-03 00:39:01 +0600

    Added Publish configuration

--- application/starter/vite.config.ts
+++ application/starter/vite.config.ts
@@ -1,5 +1,4 @@
 import {defineConfig, loadEnv} from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
@@ -15,14 +14,15 @@ export default defineConfig(({mode}) => {
 
     return {
         base: '/',
-        plugins: [react(), tsconfigPaths()],
+        plugins: [react()],
 
         server: {
             port: Number(env.PORT) || 1240,
             open: true
         },
         resolve: {
-            preserveSymlinks: true
+            preserveSymlinks: true,
+            tsconfigPaths: true,
         },
         build: {
             rollupOptions: {
