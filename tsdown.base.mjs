04a082b28bf035984156f29624b29b0735f45b3a
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-04-04 11:47:36 +0600

    Working on build system

---
+++ tsdown.base.mjs
@@ -0,0 +1,11 @@
+import {defineConfig} from 'tsdown'
+
+export const baseConfig = defineConfig({
+    format: ['esm', 'cjs'],
+    dts: true,
+    clean: true,
+    sourcemap: true,
+    minify: true,
+    target: 'es2020',
+    platform: 'neutral'
+})
