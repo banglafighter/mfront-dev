def80a097dd23ff80141b848df9b15ccea3b1511
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-04-11 23:48:11 +0600

    Working on design system

--- application/starter/src/features/example/example-all-page.tsx
+++ application/starter/src/features/example/example-all-page.tsx
@@ -4,6 +4,16 @@ export default function ExampleAllPage() {
     return (
         <>
             <div className="flex flex-wrap items-center gap-2 md:flex-row">
+
+                <Button variant="primary">Primary</Button>
+                <Button variant="danger">Danger</Button>
+                <Button variant="success">Success</Button>
+                <Button variant="link">Link</Button>
+                <Button variant="outline">Outline</Button>
+                <Button variant="info">Info</Button>
+                <Button variant="warning">Warning</Button>
+                <Button variant="secondary">Secondary</Button>
+
                 <Button type={"button"} variant={"danger"}>
                     Button
                 </Button>
