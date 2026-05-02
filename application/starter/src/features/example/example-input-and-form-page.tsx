0b13f13e6ecf4d332194128f447ed13b79f2cd93
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-03 01:42:07 +0600

    Added Input

--- application/starter/src/features/example/example-input-and-form-page.tsx
+++ application/starter/src/features/example/example-input-and-form-page.tsx
@@ -1,13 +1,26 @@
-import {InputFrame} from "mfront-ui";
+import {Input, InputFrame} from "mfront-ui";
 
 export default function ExampleInputAndFormPage() {
     return (
         <div className={"m-4"}>
             <InputFrame
                 label={"Input with label"}
+                 labelNext={"Forgot Password?"}
                 required={true}
-                element={(labelKey: string) => ("All Input Here")}
+                element={(labelKey: string) => ("All Input Here, some other text, long text")}
             />
+
+            <InputFrame
+                label={"First Name"}
+                className={"mt-4"}
+                required={true}
+                hintsText={"Please enter your first name"}
+                element={(labelKey: string) => (
+                    <Input id={labelKey} type={"text"}/>
+                )}
+            />
+
+
         </div>
     )
 }
