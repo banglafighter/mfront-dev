7d0240c69e7cbea9c73d46afd5643a412dea7c7e
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-03 15:32:02 +0600

    Working on Input Engine and Generator

--- application/starter/src/features/example/example-input-and-form-page.tsx
+++ application/starter/src/features/example/example-input-and-form-page.tsx
@@ -1,4 +1,16 @@
-import {Input, InputField, InputFrame} from "mfront-ui";
+import {
+    Button,
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    CardSubTitle,
+    CardTitle,
+    Grid,
+    Input,
+    InputField,
+    InputFrame
+} from "mfront-ui";
 
 export default function ExampleInputAndFormPage() {
     return (
@@ -17,11 +29,12 @@ export default function ExampleInputAndFormPage() {
                 hintsText={"Please enter your first name"}
                 labelNext={"Forgot Password?"}
                 element={(labelKey: string) => (
-                    <Input id={labelKey} type={"text"}/>
+                    <Input id={labelKey} type={"text"} name={"firstName"}/>
                 )}
             />
 
             <InputField
+                name={"lastName"}
                 label={"Last Name"}
                 className={"mt-4"}
                 required={true}
@@ -29,6 +42,27 @@ export default function ExampleInputAndFormPage() {
                 placeholder={"Last Name"}
                 type={"text"}
             />
+
+
+            <Card className={"mt-4"}>
+                <CardHeader>
+                    <CardTitle>This Example Form</CardTitle>
+                    <CardSubTitle>Please fill information to help us</CardSubTitle>
+                </CardHeader>
+                <CardBody>
+                    <Grid cols={12} gap={3}>
+                        <InputField name={"firstName"} type={"text"} label={"First Name"} colSpan={6} required={true}/>
+                        <InputField name={"lastName"} type={"text"} label={"Last Name"} colSpan={6}/>
+                        <InputField name={"email"} type={"text"} label={"Email"} colSpan={6} required={true}/>
+                        <InputField name={"password"} type={"text"} label={"Password"} colSpan={6} required={true}/>
+                    </Grid>
+                </CardBody>
+                <CardFooter className={"flex justify-end gap-2"}>
+                    <Button variant={"outline"}>Cancel</Button>
+                    <Button variant={"primary"}>Submit</Button>
+                </CardFooter>
+            </Card>
+
 
 
         </div>
