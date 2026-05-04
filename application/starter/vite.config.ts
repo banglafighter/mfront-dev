bd1f2a800358e3e63e7879c23f7a16ff8cc8da64
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-04 11:00:16 +0600

    working on field engine

--- application/starter/vite.config.ts
+++ application/starter/vite.config.ts
@@ -2,7 +2,6 @@ import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({mode}) => {
     const envFolder = path.resolve(__dirname, '../../env')
@@ -18,8 +17,7 @@ export default defineConfig(({mode}) => {
         base: '/',
         plugins: [
             react(),
-            tailwindcss(),
-            tsconfigPaths()
+            tailwindcss()
         ],
 
         server: {
