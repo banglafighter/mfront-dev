158a3a05c05be79fe9c78f8cbff269dfe17987f3
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-05 15:42:32 +0600

    Working on various conditions

--- application/starter/src/features/example/input/field-generator-input-view.tsx
+++ application/starter/src/features/example/input/field-generator-input-view.tsx
@@ -8,15 +8,58 @@ import {
     CardTitle, FieldGenerator,
     useFieldEngine
 } from "mfront-ui";
-import {WebFieldSpec} from "mmcore-ui";
+import {type WebFieldGroupProps, WebFieldSpec} from "mmcore-ui";
+import {Eye, EyeOff} from "lucide-react";
+import {type UINode, useRef} from "mfront";
 
 
 export default function FieldGeneratorInputView() {
     const engine = useFieldEngine()
+    const hidePassword = useRef(true)
+
+    const togglePassword = () => {
+        let spec = engine.getSpec<WebFieldGroupProps>("password")
+        if (!spec) {
+            return
+        }
+
+        if (hidePassword.current) {
+            spec.type = "text"
+            hidePassword.current = false
+        } else {
+            spec.type = "password"
+            hidePassword.current = true
+        }
+        spec.endOrButtonItems = getPasswordIcon()
+        engine.updateInputFieldSpec(spec, true)
+    }
+
+    const getPasswordIcon = () => {
+        let passwordIcon: UINode =  <Eye/>
+        if (hidePassword.current) {
+            passwordIcon =  <EyeOff/>
+        }
+        return [{itemType: "node", content: (<Button onClick={togglePassword} variant={"ghost"} size={"iconSm"}>{passwordIcon}</Button>)}]
+    }
+
     engine.registerFields((spec: WebFieldSpec) => {
         spec.text({name: "name", label: "Name", required: true, colSpan: 4, errorText: "Please enter your name"})
         spec.text({name: "fatherName", label: "Father name", colSpan: 4})
-        spec.text({name: "motherName", label: "Mother name", required: true, colSpan: 4, errorText: "Please enter your mother name"})
+        spec.text({
+            name: "motherName",
+            label: "Mother name",
+            required: true,
+            colSpan: 4,
+            errorText: "Please enter your mother name",
+        })
+        spec.password({
+            name: "password",
+            label: "Password",
+            required: true,
+            colSpan: 4,
+            errorText: "Please enter your password",
+            endOrButtonItems: getPasswordIcon()
+        })
         spec.textarea({name: "address", label: "Address", colSpan: 12})
         return spec
     })
