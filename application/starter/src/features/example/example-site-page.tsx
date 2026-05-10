1bd3d50967aab81f3de4ab8b4b6e64eea30c454f
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-10 23:01:13 +0600

    Working on select

--- application/starter/src/features/example/example-site-page.tsx
+++ application/starter/src/features/example/example-site-page.tsx
@@ -1,6 +1,14 @@
+import {Sidebar, SidebarContent, SidebarProvider, SidebarToggler} from "mfront-ui";
+
 export default function ExampleSitePage() {
     return (
         <>
+            <SidebarProvider>
+                <Sidebar/>
+                <SidebarContent>
+                    <SidebarToggler/>
+                </SidebarContent>
+            </SidebarProvider>
             Example Site Page
         </>
     )
