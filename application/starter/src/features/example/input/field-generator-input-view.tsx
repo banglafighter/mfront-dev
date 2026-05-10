47d7baf19d10e594551427189ee6ee3a2dec6cd3
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-10 15:52:34 +0600

    Working on select

--- application/starter/src/features/example/input/field-generator-input-view.tsx
+++ application/starter/src/features/example/input/field-generator-input-view.tsx
@@ -109,6 +109,7 @@ export default function FieldGeneratorInputView() {
         })
 
         spec.select({
+            placeholder: "Select country",
             label: "Select country",
             name: "country",
             options: countryOptions,
