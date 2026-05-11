402807cd2e67b2202654548aa926af04026f6ba6
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-11 16:05:22 +0600

    Working on Sidebar

--- application/starter/src/features/example/example-site-page.tsx
+++ application/starter/src/features/example/example-site-page.tsx
@@ -1,12 +1,38 @@
 import {Button, Sidebar, SidebarContent, SidebarProvider, SidebarToggler} from "mfront-ui"
 import {useState} from "mfront";
+import {type SidebarMenuItemProps} from "mmcore-ui";
+import {Cog, Component} from "lucide-react";
+
+const menuItems: SidebarMenuItemProps[] = [
+    {menuContent: "Overview"},
+    {menuContent: "Installation"},
+    {
+        menuContent: <><Cog/> Modules</>,
+        nested: [
+            {menuContent: "mFront"},
+            {menuContent: "mFront UI"},
+            {menuContent: "mFront CLI"},
+        ]
+    },
+    {
+        menuContent: "User Interface",
+        group: [
+            {
+                menuContent: (<><Component/> Components</>),
+                nested: [
+                    {menuContent: "Button"}
+                ]
+            }
+        ]
+    }
+]
 
 export default function ExampleSitePage() {
     const [isOpen, toggleOpen] = useState(true)
     return (
         <>
             <SidebarProvider >
-                <Sidebar/>
+                <Sidebar menu={menuItems} collapsible="offcanvas" variant={"inset"}/>
                 <SidebarContent>
                     <SidebarToggler/>
 
--- application/starter/src/layouts/public-layout.tsx
+++ application/starter/src/layouts/public-layout.tsx
@@ -3,7 +3,6 @@ import {PageSlot} from "mfront";
 export default function PublicLayout() {
     return (
         <>
-            Public Layout
             <div>
                 <PageSlot/>
             </div>
