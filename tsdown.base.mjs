c2e4506a020c48a2fc03239f20998c0960aeffaf
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-18 16:53:12 +0600

    Added i18n Support

--- tsdown.base.mjs
+++ tsdown.base.mjs
@@ -2,10 +2,13 @@ import {defineConfig} from 'tsdown'
 
 export const baseConfig = defineConfig({
     format: ['esm', 'cjs'],
-    dts: true,
     clean: true,
     sourcemap: true,
     minify: true,
+    css: true,
     target: 'es2020',
-    platform: 'neutral'
+    platform: 'neutral',
+    dts: {
+        eager: true
+    }
 })
