cf1657b831c15380467a7f694387c56f8f81e957
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-20 09:11:06 +0600

    Working on Release

--- tsdown.base.mjs
+++ tsdown.base.mjs
@@ -4,7 +4,7 @@ export const baseConfig = defineConfig({
     format: ['esm', 'cjs'],
     clean: true,
     sourcemap: true,
-    minify: true,
+    minify: false,
     css: true,
     target: 'es2020',
     platform: 'neutral',
