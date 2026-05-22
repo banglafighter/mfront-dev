20b0a8ea36b37e5c86fea4db2e80f8eda519eb30
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-23 00:06:58 +0600

    Working on Documentation

--- application/starter/vite.config.ts
+++ application/starter/vite.config.ts
@@ -30,21 +30,10 @@ export default defineConfig(({mode}) => {
             tsconfigPaths: true,
         },
         build: {
-            sourcemap: true,
-            rollupOptions: {
-                output: {
-                    entryFileNames: 'assets/[name].[hash].js',
-                    chunkFileNames: 'assets/[name].[hash].js',
-                    assetFileNames: 'assets/[name].[hash].[ext]',
-                    manualChunks(id) {  // Need to improve it
-                        if (id.includes('node_modules')) {
-                            return 'vendor'
-                        }
-                    }
-                }
-            }
+            sourcemap: false,
+            cssCodeSplit: true,
+            chunkSizeWarningLimit: 1000
         },
-
         define: defineEnv
     }
 })
